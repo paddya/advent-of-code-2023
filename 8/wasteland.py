a3c8cae29339be3e5bd5c2a8c37bc92d399373b1
paddya <patrick.bisenius@gmail.com>
2023-12-08 07:12:25 +0100

    Day 8.

---
+++ 8/wasteland.py
@@ -0,0 +1,37 @@
+import sys
+import re
+from collections import defaultdict
+import math
+instructions, graph = [b for b in open(sys.argv[1]).read().split('\n\n')]
+
+G = {}
+
+for node in graph.split('\n'):
+    node, left, right = re.findall('([A-Z0-9]{3}) = \(([A-Z0-9]{3}), ([A-Z0-9]{3})\)', node)[0]
+    G[node] = (left, right)
+
+currentNodes = [n for n in G.keys() if n.endswith('A')]
+#print(currentNodes)
+steps = 0
+
+current = currentNodes[0]
+
+visited = set()
+endIndex = defaultdict(list)
+
+numSteps = {}
+
+for i, start in enumerate(currentNodes):
+    steps = 0
+    visited = set()
+    current = start
+    while not current.endswith('Z'):
+        instruction = instructions[steps % len(instructions)]
+        (l, r) = G[current]
+        current = l if instruction == 'L' else r
+        steps += 1
+    numSteps[start] = steps
+    
+print(numSteps['AAA'])
+print(math.lcm(*numSteps.values()))
+    
