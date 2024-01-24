b8c8566080f98e7b68bf82d7e0e83e7d61a82c94
paddya <patrick.bisenius@gmail.com>
2024-01-24 18:04:57 +0100

    Clean up Day 23.

--- 23/walk.py
+++ 23/walk.py
@@ -3,19 +3,25 @@ from collections import deque, defaultdict
 from heapq import heappush, heappop
 from functools import cache
 
+# We use recursion later, just be safe
 sys.setrecursionlimit(100000)
 
+# Parse the input into a two-dimensional array that represents the grid
 grid = [[c for c in row] for row in open(sys.argv[1]).read().split('\n')]
 
 WIDTH = len(grid[0])
 HEIGHT = len(grid)
 
+# Tip when working with grids: it's often a lot easier to store
+# the grid as a dictionary that uses (row, column) as the key.
+# This simplifies bound checks to "(r, c) in G".
 G = dict()
 
 for r, row in enumerate(grid):
     for c, cell in enumerate(row):
         G[(r,c)] = cell
 
+# Find the start and end nodes for the search
 start = (-1, -1)
 for c, cell in enumerate(grid[0]):
     if cell == '.':
@@ -26,18 +32,43 @@ for c, cell in enumerate(grid[HEIGHT-1]):
     if cell == '.':
         end = (HEIGHT-1, c)
         break
-    
 
 
-    
-print(start, end)
+# Some general notes:
 
+# Finding the longest path in a graph is NP-hard (https://en.wikipedia.org/wiki/Longest_path_problem)
+# It is actually feasible to brute-force Part 1 because
+# the slopes constrain the possible paths in a way that keeps
+# the total number of paths manageable.
+
+
+# Translate the slopes into a direction vector
 mv = {
     '>': (0, 1),
     '^': (-1, 0),
     '<': (0, -1),
     'v': (1, 0)
 }
+
+
+# Looking at the input, we can see that the grid resembles a
+# labyrinth with many long paths between crossings.
+# Therefore, we can transform our grid into a graph that
+# only contains the crossings as nodes and weighted
+# edges that link the crossings like in the original grid.
+
+# Our first step involves finding all crossings in the graph.
+# Consider the following input:
+
+#.#####
+#...###
+###.###
+#..x..#
+#.###.#
+#####.#
+
+# A crossing is a cell that has more than two neighbors. In the example
+# input, there is only one crossing and it is marked with "x".
 
 NUM_NEIGHBORS = dict()
 CROSSINGS = set()
@@ -56,8 +87,10 @@ for (r,c), cell in G.items():
     if numNeighbors != 2:
         CROSSINGS.add((r,c))
 
+
+# findEdges() is a helper that finds the neighbors and their
+# distance for a given crossing.
 def findEdges(start, withSlopes = True):
-    
     Q = deque([(start, 0)])
     visited = set({start})
     
@@ -86,6 +119,51 @@ def findEdges(start, withSlopes = True):
                 
     return edges
 
+# edgeBfs() runs a breadth-first search that finds
+# all possible paths in the graph. In order to achieve this,
+# we store the set of visited nodes as part of the state that
+# is written to the visited set. This makes it possible to reach
+# the same spot in the grid multiple times but on different paths
+# (= a different set of already visited nodes).
+def edgeBfs(start, end, withSlopes):
+    EDGES = dict()
+    for c in CROSSINGS:
+        EDGES[c] = findEdges(c, withSlopes)
+    
+    # The initial state: the start node with distance 0 and its set of visited nodes
+    Q = deque([(start, 0, frozenset([start]))])
+    visited = set()
+    maxPath = 0
+    
+    i = 0
+    while len(Q) > 0:
+        i += 1
+        current, dist, visited = Q.popleft()        
+        
+        # Whenever we reach the end, we update our currently longest path
+        if current == end:
+            if maxPath < dist:
+                maxPath = dist
+        
+        
+        for (n, ndist) in EDGES[current]:
+            if n in G and n not in visited:
+                Q.append((n, dist+ndist, visited | {n}))
+        
+    
+    return maxPath
+
+
+print(edgeBfs(start, end, True))
+print(edgeBfs(start, end, False))
+
+
+
+# It is possible to solve Part 2 with a BFS-based approach.
+# However, exhaustive search like this are often better implemented
+# as a DFS because of the memory requirements.
+# edgeDfs() is only implemented for Part 2.
+
 EDGES_WITHOUT_SLOPES = dict()
 for c in CROSSINGS:
     EDGES_WITHOUT_SLOPES[c] = findEdges(c, False)
@@ -105,42 +183,5 @@ def edgeDfs(current, end, pathLen, visited):
     visited.discard(current)
     return maxPath
 
+
 print(edgeDfs(start, end, 0, {start}))
-    
-
-def edgeBfs(start, end, withSlopes):
-    EDGES = dict()
-    for c in CROSSINGS:
-        EDGES[c] = findEdges(c, withSlopes)
-    
-    Q = deque([(start, 0, frozenset([start]))])
-    visited = set()
-    maxPath = 0
-    D = defaultdict(int)
-    D[start] = 0
-    
-    i = 0
-    while len(Q) > 0:
-        i += 1
-        current, dist, visited = Q.popleft()
-        r, c = current
-        
-        #print(current, visited, EDGES[current])
-        
-        if current == end:
-            if maxPath < dist:
-                print('Found path: ', dist)
-
-                maxPath = dist
-        
-        
-        for (n, ndist) in EDGES[current]:
-            if n in G and n not in visited:
-                Q.append((n, dist+ndist, visited | {n}))
-        
-    
-    return maxPath
-        
-
-#print(edgeBfs(start, end, True))
-#print(edgeBfs(start, end, False))
